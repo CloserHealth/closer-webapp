c1e78ea404b6b3e675070a0c05de7c752c842e9d
godwin-olele <godwinolele10@gmail.com>
2023-10-15 15:26:38 +0100

    Fix build error

--- src/app/components/Fields/OtpInputField.tsx
+++ src/app/components/Fields/OtpInputField.tsx
@@ -18,22 +18,20 @@ const OtpInputField = ({
     <OtpInput
       value={value}
       onChange={onChange}
-      className={`font-[600] text-[20px] ${className}`}
       numInputs={4}
       renderSeparator={<span>&nbsp;&nbsp;&nbsp;</span>}
       renderInput={(props) => <input {...props} />}
       shouldAutoFocus={true}
-      hasErrored={!!error}
       containerStyle={{
         // gap: 6,
         justifyContent: 'space-between',
       }}
-      errorStyle={{
-        border: `1px solid ${alpha('#A71F0D', 0.5)}`,
-      }}
-      focusStyle={{
-        border: '1px solid #5CB67F',
-      }}
+      // errorStyle={{
+      //   border: `1px solid ${alpha('#A71F0D', 0.5)}`,
+      // }}
+      // focusStyle={{
+      //   border: '1px solid #5CB67F',
+      // }}
       inputStyle={{
         width: '50px',
         height: '56px',
@@ -41,6 +39,8 @@ const OtpInputField = ({
         // borderBottom: '1px solid #e3e3e3',
         borderRadius: "10px",
         outline: 'none',
+        fontWeight: '600',
+        fontSize: '20px'
       }}
       {...props}
     />
